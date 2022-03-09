731ccb3d0ed38249a3347aa38bc69ab38885a752
LucaDC00 <luca.de.cesare.ldc@gmail.com>
2022-03-09 16:29:59 +0100

    import components in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import Header from './components/Header.vue'
+import Navbar from './components/Navbar.vue'
 
 createApp(App).mount('#app')
 
-
+App.component('Header', Header)
+App.component('Navbar', Navbar)
