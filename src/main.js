25188a4fc9c833ad061b2e413505c2848e7f7675
LucaDC00 <luca.de.cesare.ldc@gmail.com>
2022-03-10 12:03:50 +0100

    import footer

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Header from './components/Header.vue'
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 
 createApp(App).mount('#app')
 
 App.component('Header', Header)
 App.component('Navbar', Navbar)
+App.component('Footer', Footer)
