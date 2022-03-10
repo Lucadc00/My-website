1970556c26b6f7a426f6f07e3625c431ab0c0721
LucaDC00 <luca.de.cesare.ldc@gmail.com>
2022-03-10 12:01:11 +0100

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from './components/Header.vue';
 import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Navbar/>
   <Header/>
+  <Footer/>
 
 </template>
 
